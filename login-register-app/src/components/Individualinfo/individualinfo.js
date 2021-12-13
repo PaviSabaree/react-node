29ec8f84b78ae536216c793816941fd2c14ab489
pavithra <vsabree2016@gmail.com>
2021-12-13 21:22:05 +0530

    ind-info-1

--- login-register-app/src/components/Individualinfo/individualinfo.js
+++ login-register-app/src/components/Individualinfo/individualinfo.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Header from "../Header/Header"
+import Navbar from "../Nav-bar/Nav-bar"
+import "./individualinfo.css"
 
 function Individualinfo() {
     
     return (
         <div className='ind-info'>
-            <h1>Individual Client Information</h1>
+            <Header/>
+            <Navbar/>
+            <div className='container-info'>
+                <div className='top-bar'>
+                <h7>{} Individual Client Information</h7>
+                </div>
+<div className='footer-bar'>
+    <ul>
+        <li>New</li>
+        <li>Undo</li>
+        <li>Update</li>
+        <li>Save</li>
+        <li>Documents</li>
+        <li>Exit</li>
+        <li>Print</li>
+        <li>Delete</li>
+        <li>Exit</li>
+        <li>search</li>
+       
+    </ul>
+</div>
+            </div>
+
+           
         </div>
     )
 }
